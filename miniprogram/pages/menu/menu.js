cef01e87c765a1cc7317ed931fbff46d202acf9e
Hinoki <45814922+EncreDust@users.noreply.github.com>
2021-06-26 23:48:12 +0800

    1.3.2
    修改了商品的数据类型，添加了部分功能函数

--- miniprogram/pages/menu/menu.js
+++ miniprogram/pages/menu/menu.js
@@ -2,9 +2,10 @@
 Page({
 
   data: {
-    // 商品对象
-    GoodsInfo: {},
-
+    // 商品对象数据结构
+    /*
+      商品ID goods_id | 商品名称 goods_name |  商品价格 goods_price | 商品缩略图 imgSrc 
+    */
 
     // banner的轮播图数组
     swiperArr: [
@@ -13,133 +14,158 @@ Page({
       "/images/banner/menu_banner_3.jpg"
     ],
 
-    productArr: [{
-      "id": 0,
-      "title": "人气咖啡Top",
-      "subArr": [
-       { imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/csht_nt/sight.jpg",
-       imgDesc:"冲绳黑糖拿铁",
-       money:"14"
+    // 商品数组
+    //  商品类别ID cat_id | 商品类别名称 cat_title | 商品数组 goodsArr
+    productArr: [
+      {
+        "cat_id": 0,
+        "cat_title": "人气咖啡Top",
+        "goodsArr": [
+            { 
+              goods_id: "1",
+              goods_name:"冲绳黑糖拿铁",
+              goods_price:"14",
+              imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/csht_nt/sight.jpg" 
+            },
+            {
+              goods_id: "2",
+              goods_name:"冰厚乳拿铁",
+              goods_price:"13",
+              imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ice_hr_nt/sight.jpg"
+            },
+            {
+              goods_id: "3",
+              goods_name:"焦糖拿铁",
+              goods_price:"15",
+              imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jt_nt/sight.jpg"
+              
+            },
+            {
+              goods_id: "4",
+              goods_name:"标准美式",
+              goods_price:"10",
+              imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/bz_ms/sight.jpg"
+            }
+          ]
+        },
+        {
+        "cat_id": 1,
+        "cat_title": "生椰家族",
+        "goodsArr": [ 
+          {
+            goods_id: "5",
+            goods_name:"云南.生椰拿铁",
+            goods_price:"14",
+            imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/yn_sy_nt/sight.jpg"  
+          },
+          {
+            goods_id: "6",
+            goods_name:"陨石生椰拿铁",
+            goods_price:"13",
+            imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/yssy_nt/sight.jpg"      
+          }
+        ]
       },
       {
-        imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ice_hr_nt/sight.jpg",
-        imgDesc:"冰厚乳拿铁",
-        money:"13"
-      },
-      { imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jt_nt/sight.jpg",
-      imgDesc:"焦糖拿铁",
-      money:"15"
-    },
+        "cat_id": 2,
+        "cat_title": "夏日冰咖",
+        "goodsArr": [ 
+          {
+            goods_id: "7",
+            goods_name:"西柚冰萃咖啡",
+            goods_price:"9",            
+            imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/xybc_kf/sight.jpg"
+          }
+        ]
+     },
     {
-      imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/bz_ms/sight.jpg",
-      imgDesc:"标准美式",
-      money:"10"
-    }
-      ]
-    },
-    {
-      "id": 1,
-      "title": "生椰家族",
-      "subArr": [ 
+      "cat_id": 3,
+      "cat_title": "瑞纳冰",
+      "goodsArr": [
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/yn_sy_nt/sight.jpg",
-          imgDesc:"云南.生椰拿铁",
-          money:"14"
+          goods_id: "8",
+          goods_name:"陨石拿铁瑞纳冰",
+          goods_price:"10",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ysnt_rnb/sight.jpg"
         },
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/yssy_nt/sight.jpg",
-          imgDesc:"陨石生椰拿铁",
-          money:"13"
+          goods_id: "9",
+          goods_name:"椰子OK瑞纳冰",
+          goods_price:"11",
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/yzok_rnb/sight.jpg"
         }
       ]
     },
     {
-      "id": 2,
-      "title": "夏日冰咖",
-      "subArr": [ 
+      "cat_id": 4,
+      "cat_title": "大师咖啡",
+      "goodsArr": [
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/xybc_kf/sight.jpg",
-          imgDesc:"西柚冰萃咖啡",
-          money:"9"
-        }
-      ]
-    },
-    {
-      "id": 3,
-      "title": "瑞纳冰",
-      "subArr": [
-        {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ysnt_rnb/sight.jpg",
-          imgDesc:"陨石拿铁瑞纳冰",
-          money:"10"
+          goods_id: "10",
+          goods_name:"冰海盐芝士拿铁",
+          goods_price:"15",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ice_hyzs_nt/sight.jpg",   
         },
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/yzok_rnb/sight.jpg",
-          imgDesc:"椰子OK瑞纳冰",
-          money:"11"
-        }
-      ]
-    },
-    {
-      "id": 4,
-      "title": "大师咖啡",
-      "subArr": [
-        {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ice_hyzs_nt/sight.jpg",
-          imgDesc:"冰海盐芝士拿铁",
-          money:"15"
-        },
-        {
+          goods_id: "11",
+          goods_name:"冰桃桃厚乳拿铁",
+          goods_price:"15",          
           imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ice_tthr_nt/sight.jpg",
-          imgDesc:"冰桃桃厚乳拿铁",
-          money:"15"
+         
         },
         {
+          goods_id: "12",
+          goods_name:"姜饼风味拿铁",
+          goods_price:"14",          
           imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jbr_nt/sight.jpg",
-          imgDesc:"姜饼风味拿铁",
-          money:"14"
         },
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jtbz_ms/sight.jpg",
-          imgDesc:"焦糖标准美式",
-          money:"10"
+          goods_id: "13",
+          goods_name:"焦糖标准美式",
+          goods_price:"10",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jtbz_ms/sight.jpg",   
         },
-      
         {
+          goods_id: "14",
+          goods_name:"摩卡",
+          goods_price:"12",          
           imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/mk/sight.jpg",
-          imgDesc:"摩卡",
-          money:"12"
         },
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ttym_nt/sight.jpg",
-          imgDesc:"桃桃燕麦拿铁",
-          money:"13"
+          goods_id: "15",
+          goods_name:"桃桃燕麦拿铁",
+          goods_price:"13",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ttym_nt/sight.jpg",    
         },
         {
-          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ynhm_arb/sight.jpg",
-          imgDesc:"云南红蜜·澳瑞白",
-          money:"16"
+          goods_id: "16",
+          goods_name:"云南红蜜·澳瑞白",
+          goods_price:"16",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/ynhm_arb/sight.jpg", 
         },
       ]
     },
     {
-      "id": 5,
-      "title": "经典拿铁",
-      "subArr": [ {
-        imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/zg_nt/sight.jpg",
-        imgDesc:"榛果拿铁",
-        money:"10"
-      },
-      {
-        imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jt_nt/sight.jpg",
-        imgDesc:"焦糖拿铁",
-        money:"11"
-      },
-      {
-        imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/kbqn/sight.jpg",
-        imgDesc:"卡布奇诺",
-        money:"13"
-      }
+      "cat_id": 5,
+      "cat_title": "经典拿铁",
+      "goodsArr": [ {
+        goods_id: "17",
+        goods_name:"榛果拿铁",
+        goods_price:"10",        
+        imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/zg_nt/sight.jpg",        
+        },
+        {
+          goods_id: "18",
+          goods_name:"焦糖拿铁",
+          goods_price:"11",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/jt_nt/sight.jpg",          
+        },
+        {
+          goods_id: "18",
+          goods_name:"卡布奇诺",
+          goods_price:"13",          
+          imgSrc:"cloud://coffee-6gdizycb4a945201.636f-coffee-6gdizycb4a945201-1306337290/images/menu/products/kbqn/sight.jpg",    
+        }
       ]
     }
   ],
@@ -153,6 +179,8 @@ Page({
     // 右侧高度的数组
     heightArr: []
   },
+
+  GoodsInfo: {},
 
   // 获取每个区块的高度(操作dom),声明周期
   onReady() {
@@ -189,7 +217,7 @@ Page({
 
   // 右侧滚动事件
   rightScrollFn(e) {
-    console.log(e.detail.scrollTop)
+    // console.log(e.detail.scrollTop)
     let st = e.detail.scrollTop;
     let myarr = this.data.heightArr;
     for(let i=0; i<myarr.length; i++){
@@ -206,7 +234,7 @@ Page({
   // 点击并添加商品到购物车
   handleCartAdd(e) {
     // 获取缓存中的购物车数组
-    let carts = wx.getStorageSync('cart') || [];    // || [] 转换格式, 第一次获取是空的
+    let cart = wx.getStorageSync('cart') || [];    // || [] 转换格式, 第一次获取是空的
     // 判断商品对象是否存在于购物车数组中
     let index = cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
     if(index===-1) {
@@ -217,9 +245,33 @@ Page({
       // 已经存在购物车数据 执行 num++
       cart[index].num++;
     }
+    console.log(this.GoodsInfo.basicInfo);
+    // 弹窗提示
+    wx.showToast({
+      title: 'Enjoy',
+      icon: 'success',
+      // true 防止用户持续点击
+      mask: true,
+      success:(result) => {
+
+      },
+      fail: () => {},
+      complete:() => {}
+    });
 
     // 将购物车添加回缓存中
     wx.setStorageSync('cart', cart);
-  }
+  },
+
+
+
+  /*
+      ！！！！  未实现功能 ！！！！
+  */
+
+  // 获取商品详细数据
+  // async getGoodsDetail(goods_id) {
+  //   const res = await request({url:"/goods/", data:{goods_id}});     // "" 中填写接口
+  // }
 
 })
--- miniprogram/pages/shoppingcart/shoppingcart.js
+++ miniprogram/pages/shoppingcart/shoppingcart.js
@@ -6,14 +6,100 @@ Page({
    */
   data: {
     // 购物车中的数据
-    cart: []
+    cart: [],
+    allChecked: false,
+    totalPrice: 0,
+    totalNum: 0
   },
 
   // 获取缓存中的数据
   onShow() {
     // 获取缓存中的购物车数据
-    const cart = wx.getStorageSync('cart');
-  }
+    const cart = wx.getStorageSync('cart') || [];
+    this.setData(cart)
+  },
 
+  // 商品被选中
+  handleItemChange(e) {
+    // 获取被修改的商品的id
+    const goods_id = e.currentTarget.dataset.id;
+    // 获取购物车数组
+    let { cart } = this.data;
+    // 找到被修改的商品对象
+    let index = cart.findIndex(v => v.goods_id === goods_id);
+    // 选中状态取反
+    this.setCart(cart);
+  },
+
+  // 设置购物车状态的同时，重新计算底部工具栏中的数据  全选 | 总价格 | 购买数量
+  setCart(cart) {
+    let allChecked = true;
+    // 总价格
+    let totalPrice = 0;
+    // 总数量
+    let totalNum = 0
+    cart.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      } else {
+        allChecked = false;
+      }
+    })
+    // 判断数组是否为空
+    allChecked = cart.length != 0 ? allChecked : false;
+    this.setData({
+      cart,
+      totalPrice, totalNum, allChecked
+    });
+    // 重新存入缓存中
+    wx.setStorageSync('cart', cart);
+  },
+
+  // 商品全选功能
+  handleItemCheck() {
+    // 获取data中的数据
+    let { cart, allChecked } = this.data;
+    // 修改值
+    allChecked = !allChecked;
+    // 循环修改cart数组中的商品选中状态
+    cart.forEach(v => v.checked = allChecked);
+    // 把修改后的值填充回data和缓存中
+    this.setData(cart);
+  },
+
+  // 商品数量的编辑功能
+  async handleItemNumEdit(e) {
+    // 获取传递过来的参数
+    const { operation, id} = e.currentTarget.dataset;
+    // 获取购物车数组
+    let { cart } = this.data;
+    // 找到需要修改的商品的索引
+    const index = cart.findIndex(v => v.goods_id === id);
+    // 判断是否要执行删除
+    if(cart[index].num === 1 && operation === -1) {
+      // 弹窗提示
+      const res = await wx.showModal({
+        content: "您是否要删除?"
+      });
+      if(res.confirm) {
+        cart.splice(index, 1);
+        this.setCart(cart);   // 重新写回缓冲和data中
+      } else {
+        // 进行修改数量
+        cart[index].num += operation;
+        // 重新写回缓冲和data中
+        this.setCart(cart);
+      }
+    }
+  },
+
+  // 点击结算
+  async handlePay() {
+    // 跳转到支付页面
+    wx.navigateTo({
+      url: '/pages/pay/pay',
+    });
+  }
 
 })
